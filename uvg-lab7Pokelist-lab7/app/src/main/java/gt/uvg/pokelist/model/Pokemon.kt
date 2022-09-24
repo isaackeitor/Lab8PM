b978bcfa8380c87b43376f491c420d790683d5d5
isaackeitor <josuem81010@gmail.com>
2022-09-24 17:09:55 -0600

    Cambios para el uso de Retrofit y Moshi

--- uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/model/Pokemon.kt
+++ uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/model/Pokemon.kt
@@ -1,7 +1,10 @@
 package gt.uvg.pokelist.model
 
+import com.squareup.moshi.Json
+
 data class Pokemon(
     val id: Int,
+    @Json(name = "name")
     val name: String
 ) {
     val imageUrlFront: String get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
@@ -9,3 +12,5 @@ data class Pokemon(
     val imageUrlShinnyFront: String get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/$id.png"
     val imageUrlShinnyBack: String get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/$id.png"
 }
+
+data class PokemonResponse(@Json(name="results") val result : List<Pokemon>)

30829cd723f0af3b31ec03f2d49d0294abbab807
isaackeitor <josuem81010@gmail.com>
2022-09-24 14:25:28 -0600

    CODIGO BASE

---
+++ uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/repository/PokemonRepository.kt
@@ -0,0 +1,26 @@
+package gt.uvg.pokelist.repository
+
+import gt.uvg.pokelist.model.Pokemon
+
+class PokemonRepository {
+
+    // Obtain pokemon list from https://pokeapi.co/
+    fun getPokemonList(): List<Pokemon> {
+        return listOf(
+            Pokemon(1, "bulbasaur"),
+            Pokemon(2, "ivysaur"),
+            Pokemon(3, "venusaur"),
+            Pokemon(4, "charmander"),
+            Pokemon(5, "charmeleon"),
+            Pokemon(6, "charizard"),
+            Pokemon(7, "squirtle"),
+            Pokemon(8 , "wartortle"),
+            Pokemon(9, "blastoise"),
+            Pokemon(10,"caterpie"),
+            Pokemon( 11, "metapod"),
+            Pokemon( 12, "butterfree"),
+            Pokemon( 13, "weedle"),
+            Pokemon( 14, "kakuna")
+        )
+    }
+}
---
+++ uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/view/MainActivity.kt
@@ -0,0 +1,36 @@
+package gt.uvg.pokelist.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import gt.uvg.pokelist.R
+import gt.uvg.pokelist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // Make sure actions in the ActionBar get propagated to the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+}

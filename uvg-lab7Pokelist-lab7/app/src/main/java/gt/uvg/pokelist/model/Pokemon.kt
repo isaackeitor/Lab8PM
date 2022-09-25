c965ead5f97a3010d972397dbaad54f1ab2ea1ed
isaackeitor <josuem81010@gmail.com>
2022-09-24 23:31:41 -0600

    Corrección de generación de id para cada pokemon

--- uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/model/Pokemon.kt
+++ uvg-lab7Pokelist-lab7/app/src/main/java/gt/uvg/pokelist/model/Pokemon.kt
@@ -6,21 +6,9 @@ data class Pokemon(
     @Json(name = "url")
     val url: String,
     @Json(name = "name")
-    val name: String
+    val name: String,
+    val id: Int = url[url.length - 2].digitToInt()
 ) {
-    private fun removefirstNchars(s: String?, n: Int): String? {
-        return if (s == null || s.length < n) {
-            s
-        } else s.substring(n)
-    }
-    private val url2: String = removefirstNchars(url,34)!!
-
-    private fun removeLastNchars(str: String, n: Int): String {
-        return str.substring(0, str.length - n)
-    }
-
-    val id: Int = Integer.valueOf(removeLastNchars(url2, 1))
-
     val imageUrlFront: String get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
     val imageUrlBack: String get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/$id.png"
     val imageUrlShinnyFront: String get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/$id.png"
